1afc21ca885bf75053a2c749827b0cf5637f7f01
Manas Ranjan Pati <iammanas24@gmail.com>
2020-11-17 18:39:31 +0530

    Generic Viewsets

--- polls/apiview.py
+++ polls/apiview.py
@@ -1,6 +1,9 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 
 from .models import Poll, Choice
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 from .serializers import PollSerializer, ChoiceSerializer,\
     VoteSerializer
@@ -11,15 +14,31 @@ class PollList(generics.ListCreateAPIView):
     serializer_class = PollSerializer
 
 
+class PollViewSet(viewsets.ModelViewSet):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
 class PollDetail(generics.RetrieveDestroyAPIView):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
 
 
 class ChoiceList(generics.ListCreateAPIView):
-    queryset = Choice.objects.all()
+    def get_queryset(self):
+        queryset = Choice.objects.filter(poll_id=self.kwargs["pk"])
+        return queryset
     serializer_class = ChoiceSerializer
 
 
-class CreateVote(generics.CreateAPIView):
-    serializer_class = VoteSerializer
+class CreateVote(APIView):
+
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get("voted_by")
+        data = {'choice': choice_pk, 'poll': pk, 'voted_by': voted_by}
+        serializer = VoteSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from .views import polls_list, polls_detail
 
 # DRF
-from .apiview import PollList, PollDetail, ChoiceList, CreateVote
+from .apiview import PollList, PollDetail, ChoiceList, CreateVote, PollViewSet
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register('polls', PollViewSet, basename='polls')
+
 
 urlpatterns = [
     path("polls/", polls_list, name="polls_list"),
@@ -11,7 +15,12 @@ urlpatterns = [
     # DRF
     path("api/polls/", PollList.as_view(), name="polls_list"),
     path("api/polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
-    path("choices/", ChoiceList.as_view(), name="choice_list"),
-    path("vote/", CreateVote.as_view(), name="create_vote"),
+    # path("choices/", ChoiceList.as_view(), name="choice_list"),
+    # path("vote/", CreateVote.as_view(), name="create_vote"),
+    path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
+
 
 ]
+
+urlpatterns += router.urls
