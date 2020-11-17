ea7d242aa847e7e8d366d312358ed2983a6ab649
Manas Ranjan Pati <iammanas24@gmail.com>
2020-11-17 19:25:29 +0530

    setup test cases and final touch

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+
+
+# class TestPoll(APITestCase):
+#     def setUp(self):
+#         # ...
+#         self.user = self.setup_user()
+#         self.token = Token.objects.create(user=self.user)
+#         self.token.save()
+
+#     @staticmethod
+#     def setup_user():
+#         User = get_user_model()
+#         return User.objects.create_user(
+#             'test',
+#             email='testuser@test.com',
+#             password='test'
+#         )
+
+#     def test_list(self):
+#         request = self.factory.get(self.uri,
+#                                    HTTP_AUTHORIZATION='Token {}'.format(self.token.key))
+#         request.user = self.user
+#         response = self.view(request)
+#         self.assertEqual(response.status_code, 200,
+#                          'Expected Response Code 200, received {0} instead.'
+#                          .format(response.status_code))
+
+
+
+class TestPoll(APITestCase):
+    # ...
+
+    def test_list2(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get(self.uri)
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code))
