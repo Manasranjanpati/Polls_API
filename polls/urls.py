66410ff7c335cc0dd5f3578c526c15606184971a
Manas Ranjan Pati <iammanas24@gmail.com>
2020-11-17 18:16:38 +0530

    drf api view for CRUD

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from .views import polls_list, polls_detail
 
+# DRF
+from .apiview import PollList, PollDetail, ChoiceList, CreateVote
+
 urlpatterns = [
     path("polls/", polls_list, name="polls_list"),
-    path("polls/<int:pk>/", polls_detail, name="polls_detail")
+    path("polls/<int:pk>/", polls_detail, name="polls_detail"),
+
+    # DRF
+    path("api/polls/", PollList.as_view(), name="polls_list"),
+    path("api/polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    path("choices/", ChoiceList.as_view(), name="choice_list"),
+    path("vote/", CreateVote.as_view(), name="create_vote"),
+
 ]
