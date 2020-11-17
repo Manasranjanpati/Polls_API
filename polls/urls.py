67f02c0070dd05a7487b7e9924022595e9cf5eaa
Manas Ranjan Pati <iammanas24@gmail.com>
2020-11-17 18:59:26 +0530

    Poll Apis modification with proper urls dispatching

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import polls_list, polls_detail
+# from rest_framework.authtoken import views
+
 
 # DRF
-from .apiview import PollList, PollDetail, ChoiceList, CreateVote, PollViewSet
+from .apiview import PollList, PollDetail, ChoiceList, CreateVote, \
+    PollViewSet, UserCreate, LoginView
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('polls', PollViewSet, basename='polls')
@@ -15,10 +18,17 @@ urlpatterns = [
     # DRF
     path("api/polls/", PollList.as_view(), name="polls_list"),
     path("api/polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+
+    # User
+    path("login/", LoginView.as_view(), name="login"),
+    # path("login/", views.obtain_auth_token, name="login"), #Another way to create Login endpoint
+    path("users/", UserCreate.as_view(), name="user_create"),
+
     # path("choices/", ChoiceList.as_view(), name="choice_list"),
     # path("vote/", CreateVote.as_view(), name="create_vote"),
     path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
-    path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/",
+         CreateVote.as_view(), name="create_vote"),
 
 
 ]
